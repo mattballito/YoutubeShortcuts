8dca02be28446cf2a45c9a13e6a55778b0d17ea6
mattballito <mbutner@csu.fullerton.edu>
2024-06-04 17:56:20 -0700

    background listener

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onMessage.addListener(function(request) {
+    if (request.controlledTabId) {
+      chrome.storage.local.set({controlledTabId: request.controlledTabId});
+    }
+  });
+
+chrome.commands.onCommand.addListener(function(command) {
+    chrome.storage.local.get(['controlledTabId'], function(result) {
+        console.log("this is the command:")
+        console.log(command)
+      if (result.controlledTabId) {
+        chrome.tabs.sendMessage(result.controlledTabId, {command: command});
+      } else {
+        console.log('No controlled tab found.');
+      }
+    });
+  });
+  
